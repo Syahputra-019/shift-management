710d2f786b49c5fc6e3a354df0be5801cd6fa3a6
putra <syahputratirta.001@gmail.com>
2026-08-14 19:52:53 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ScheduleController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
@@ -11,7 +12,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/swap-shift', [DashboardController::class, 'swapShiftPage'])->name('swap-shift.index');
     Route::post('/swap-requests/{id}/respond', [DashboardController::class, 'respondSwapRequest'])->name('swap-requests.respond');
     Route::post('/swap-requests', [DashboardController::class, 'submitSwapRequest'])->name('swap-requests.store');
-    
+    Route::get('/employees', [EmployeeController::class, 'index'])->name('employees.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
