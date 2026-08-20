d47053010ab5aa9a8bfb1f82615dfae98894d1a7
putra <syahputratirta.001@gmail.com>
2026-08-20 21:53:49 +0700

    up

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -104,7 +104,7 @@
                             d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M15 21a6 6 0 00-9-5.197m0 0A6.995 6.995 0 0012 12.75a6.995 6.995 0 00-3-5.197M15 21a9 9 0 00-9-9">
                         </path>
                     </svg>
-                    <span>Karyawan</span>
+                    <span>Karyawan </span>
                 </a>
                 <a href="{{ route('reports.index') }}"
                     class="{{ request()->routeIs('reports.*') ? 'bg-blue-50 font-semibold text-blue-600' : 'font-medium text-slate-600 hover:bg-slate-100 hover:text-slate-900' }} flex items-center rounded-lg px-4 py-2.5 transition-colors">
